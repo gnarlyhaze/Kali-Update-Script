b03a1141ab44abb6c183d95e29152110a3e7ee2b
gnarlyhaze <nathanclifford123@hotmail.com>
2018-02-27 21:12:11 -0500

    Update usershell.sh

--- usershell.sh
+++ usershell.sh
@@ -3,8 +3,11 @@
 # This script opens a terminal window titled with the current username with the ~/Desktop of the directory.
 
 # Pass username to terminal title as a variable.
-#USER=Nathan #Hard-code your own name here instead of using the whoami command to discover the current username (which in Kali is probably just root)
+#USER=Nathan #Hard-code your own name here or use the whoami command to discover the current username (which in Kali is probably just root)
 USER=$(whoami)
 
 # Open a terminal with the current username as the title.
 x-terminal-emulator --working-directory=~/Desktop --title="$USER's Shell" 
+
+# Different formatting - username@hostname
+#x-terminal-emulator --working-directory=~/Desktop --title="${USER}@${HOSTNAME}"
