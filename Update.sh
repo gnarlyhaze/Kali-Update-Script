9650cb51015ef15b5954db4ab8cebd148776ee8e
gnarlyhaze <nathanclifford123@hotmail.com>
2018-03-03 02:09:52 -0500

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -143,9 +143,10 @@ sudo apt install -y autocutsel #allows copy and paste text between applications
 # update-alternatives --config x-terminal-emulator #set a default terminal emulator program
 echo ""
 printf "${LGREEN}Installing Terminal applications and Alternate Shells...${NC}\n"
-sudo apt install -y terminator # My favorite Terminal emulator
-sudo apt install -y lxterminal # Another solid Terminal emulator
-sudo apt install -y --install-suggests guake #Drop down terminal emulator used with F12 - Also a favorite
+#sudo apt install -y terminator # My favorite Terminal emulator
+#mkdir /root/.config/terminator/config && cd /root/.config/terminator/config && touch config #configure terminator
+#sudo apt install -y lxterminal # Another solid Terminal emulator
+#sudo apt install -y --install-suggests guake #Drop down terminal emulator used with F12 - Also a favorite
 #sudo apt install --install-suggests yakuake #Light-weight drop down terminal emulator used with F12
 sudo apt install -y --install-suggests undistract-me #notifications that watch for long running commands and create a popup when complete
 
@@ -339,8 +340,6 @@ sudo apt install -y scrot
 sudo apt install -y imagemagick
 sudo apt install -y shutter
 #sudo apt install -y gtk-recordmydesktop recordmydesktop #Screen recorder software
-
-
 
 ###Linux Window Managers 
 #update-alternatives --config x-window-manager #command to allow you to choose your window manager. The options so far are gdm3, xfce4, and awesome, MATE, and i3.
