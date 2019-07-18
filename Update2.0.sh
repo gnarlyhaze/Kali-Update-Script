f7bbdbfccd9fb81b5894aa13f4c44845de50e192
gnarlyhaze <nathanclifford123@hotmail.com>
2019-07-17 22:00:04 -0400

    Update Update2.0.sh

--- Update2.0.sh
+++ Update2.0.sh
@@ -232,6 +232,7 @@ sudo apt install -y zsh #An extended Bourne shell
 ### Install Utility applications
 echo ""
 printf "${LGREEN}Installing Utility applications...${NC}\n"
+sudo apt install -y needrestart needrestart-session #check which daemons need to be restarted after library upgrades
 sudo apt install -y gparted #gparted disk utility
 sudo apt install -y speedtest-cli #speedtest from command line
 sudo apt install -y gdebi #GUI for installing .deb files
