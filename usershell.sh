8c7cfafcc22705860123aa85250048a829e93c3d
gnarlyhaze <nathanclifford123@hotmail.com>
2020-08-04 14:32:26 -0400

    Update usershell.sh

--- usershell.sh
+++ usershell.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Created by Nathan Clifford
+
 # This script opens a terminal window titled with the current username with the ~/Desktop of the directory.
 
 # Pass username to terminal title as a variable.
-#USER=Nathan #Hard-code your own name here or use the whoami command to discover the current username (which in Kali is probably just root)
+#USER=Gnarlyhaze #Hard-code your own name here or use the whoami command to discover the current username (which in Kali is probably just root)
 USER=$(whoami)
 
 # Open a terminal with the current username as the title.
